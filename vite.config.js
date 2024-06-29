f10902338de59ea9e32f46567e0e4e800583451f
RobertSajdak <robert.sajdak@onet.eu>
2024-06-29 20:40:49 +0200

    social media init

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { resolve } from "path";
+import handlebars from "vite-plugin-handlebars";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
+
+export default {
+  assetsInclude: ["./assets/img/**/*"],
+  server: {
+    open: "./index.html",
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: "./index.html",
+      },
+    },
+  },
+  plugins: [
+    handlebars({
+      partialDirectory: resolve(__dirname, "partials"),
+      reloadOnPartialChange: true,
+    }),
+    ViteImageOptimizer(),
+  ],
+};
